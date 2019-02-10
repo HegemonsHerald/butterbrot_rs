2a022711d18738878e8eb2d61f6eddcc6a88f24d
HegemonsHerald <benediktheinen1@gmail.com>
2019-02-10 02:43:31 +0100

    added the data sets, that led to great good.

--- src/birb2term.rs
+++ src/birb2term.rs
@@ -9,6 +9,25 @@ fn main() {
     let w = birb[0];
     let h = birb[1];
 
+
+
+	/* The good parts.
+	 *
+	 * Render 'Honk4.birb' using the first section's for loops.
+	 * This one's the first time, I made the Buddhabrot apparent.
+	 *
+	 * Render 'Honk7.birb' using the second section's for loops.
+	 * This one's gotten about 20 times the resolution of honk4.
+	 *
+	 */
+
+
+
+
+    // good render settings:
+    // --samples 10_000_000 --iterations 200 and whatever warmup it was, that I set as default right now (I think 400, or maybe 10)
+
+	// works well with data set 'honk4' -- the first truly successfull run!
     for i in 0..h {
 
         for j in 2..w {
@@ -29,6 +48,38 @@ fn main() {
 			print!("▓▓▓▓▓▓▓▓");
 		} else if c > 20000 {
 			print!("████████");
+		}
+            // print!("{:^10}", (birb[(i*w+j) as usize] as f64).sqrt() as u64);
+            // print!("{:^10}", (birb[(i*w+j) as usize]) as u64);
+
+        }
+
+        print!("\n");
+
+    }
+
+    for i in 0..h {
+
+        for j in 2..w {
+
+		let c = birb[(i*w+j-2) as usize];
+		if c > 50 && c < 150 {
+			print!(" ∙ ");
+		} else if c > 150 && c < 1000 {
+			print!(" ○ ");
+		} else if c > 500 && c < 1000 {
+			print!(" ◆ ");
+		} else if c > 1000 && c < 5000 {
+			print!("░░░");
+		} else if c > 5000 && c < 10000 {
+			print!("▒▒▒");
+			// print!("{:^10}", c);
+		} else if c > 10000 && c < 20000{
+			print!("▓▓▓");
+		} else if c > 20000 {
+			print!("███");
+		} else {
+			print!("   ");
 		}
             // print!("{:^10}", (birb[(i*w+j) as usize] as f64).sqrt() as u64);
             // print!("{:^10}", (birb[(i*w+j) as usize]) as u64);
